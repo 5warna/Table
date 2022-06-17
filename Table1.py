dd5b6bbce8cb76fed36ebd498a71a2e6fa06304a
Kannappan Sriramajayam <you@example.com>
2022-06-17 17:41:29 -0400

    Multiplication Table of Numbers Using for and while loop

---
+++ Table1.py
@@ -0,0 +1,4 @@
+#Multiplication table using for loop
+n = int(input("Enter the number: "))
+for i in range(1, 11):
+    print(f'{i} * {n} = {i * n}')
---
+++ Table2.py
@@ -0,0 +1,6 @@
+#Multiplication Table using while loop
+n = int(input("Enter the number: "))
+i = 1
+while i <= 10:
+    print(f'{i}*{n} = {i * n}')
+    i = i + 1
